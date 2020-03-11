9fffeae83a606bac9e9b317a8682537f2f44ca16
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-03-11 22:19:44 +0300

    `throwOnlyPlayerCan` and `throwPermissionLevel` added in ModErrorsHelper.kt.
    `ONLY_PLAYER_CAN` and `PERMISSION_LEVEL` in ModErrorsHelper.kt now is deprecated.
    
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/helpers/ModErrorsHelper.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/helpers/ModErrorsHelper.kt
@@ -2,14 +2,18 @@
 
 package com.mairwunnx.projectessentials.core.helpers
 
+import org.apache.logging.log4j.LogManager
+
 /**
  * Error message with reason: not have permissions.
  * @since 1.14.4-1.0.0.0
  */
+@Deprecated("Instead this use `throwPermissionLevel` method.")
 const val PERMISSION_LEVEL =
     "Player (%0) failed to executing \"/%1\" command".plus(
         "\n    - Reason: permission level executing command more than player permission level."
     )
+
 /**
  * Error message with reason: command cooldown not expired.
  * @since 1.14.4-1.0.0.0
@@ -18,6 +22,7 @@ const val COOLDOWN_NOT_EXPIRED =
     "Player (%0) failed to executing \"/%1\" command".plus(
         "\n    - Reason: command cooldown not expired."
     )
+
 /**
  * Error message with reason: typed command disabled.
  * @since 1.14.4-1.0.0.0
@@ -26,6 +31,7 @@ const val DISABLED_COMMAND =
     "Player (%0) failed to executing \"/%1\" command".plus(
         "\n    - Reason: it command disabled by mod configuration."
     )
+
 /**
  * Error message with reason: disabled command argument.
  * @since 1.14.4-1.0.0.0
@@ -34,13 +40,48 @@ const val DISABLED_COMMAND_ARG =
     "Player (%0) failed to executing \"/%1\" command".plus(
         "\n    - Reason: arguments for it command disabled by mod configuration."
     )
+
 /**
  * Error message with reason: only player can execute command.
  * @since 1.14.4-1.0.0.0
  */
+@Deprecated("Instead this use `throwOnlyPlayerCan` method.")
 const val ONLY_PLAYER_CAN =
     "Server failed to executing \"/%0\" command".plus(
         "\n    - Reason: command should only be used by the player."
     ).plus(
         "\n    - Solution: try use command with argument or target."
     )
+
+private val logger = LogManager.getLogger()
+
+/**
+ * Throw in console warning message with reason:
+ * only player can execute command.
+ *
+ * @param command executed command name.
+ *
+ * @since 1.14.4-1.3.0
+ */
+fun throwOnlyPlayerCan(command: String) = logger.warn(
+    "\n  Server failed to executing \"/$command\" command".plus(
+        "\n    - Reason: command should only be used by the player."
+    ).plus(
+        "\n    - Solution: try use command with argument or target."
+    )
+)
+
+/**
+ * Throw in console warning message with reason:
+ * player don't have permissions.
+ *
+ * @param playerName player nickname represented as string.
+ * @param command executed command name.
+ *
+ * @since 1.14.4-1.3.0
+ */
+fun throwPermissionLevel(playerName: String, command: String) = logger.warn(
+    "Player ($playerName) failed to executing \"/$command\" command".plus(
+        "\n    - Reason: permission level executing command more than player permission level."
+    )
+)
