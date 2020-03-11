bf6f65dc68de0b92dad2fdd1ec26da195b015106
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-03-11 20:01:58 +0300

    Clear have got safe localization.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/vanilla/commands/ClearCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/vanilla/commands/ClearCommand.kt
@@ -9,6 +9,9 @@ package com.mairwunnx.projectessentials.core.vanilla.commands
 import com.mairwunnx.projectessentials.cooldown.essentials.CommandsAliases
 import com.mairwunnx.projectessentials.core.EntryPoint
 import com.mairwunnx.projectessentials.core.configuration.commands.CommandsConfigurationUtils
+import com.mairwunnx.projectessentials.core.configuration.localization.LocalizationConfigurationUtils
+import com.mairwunnx.projectessentials.core.extensions.hoverEventFrom
+import com.mairwunnx.projectessentials.core.extensions.textComponentFrom
 import com.mairwunnx.projectessentials.core.helpers.PERMISSION_LEVEL
 import com.mairwunnx.projectessentials.core.vanilla.utils.NativeCommandUtils
 import com.mojang.brigadier.CommandDispatcher
@@ -24,7 +27,6 @@ import net.minecraft.entity.player.ServerPlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.util.text.Style
 import net.minecraft.util.text.TranslationTextComponent
-import net.minecraft.util.text.event.HoverEvent
 import org.apache.logging.log4j.LogManager
 import java.util.function.Function
 import java.util.function.Predicate
@@ -119,15 +121,17 @@ internal object ClearCommand {
                         .replace("%1", "clear")
                 )
                 throw CommandException(
-                    TranslationTextComponent(
+                    textComponentFrom(
+                        source.asPlayer(),
+                        LocalizationConfigurationUtils.getConfig().enabled,
                         "native.command.restricted"
                     ).setStyle(
                         Style().setHoverEvent(
-                            HoverEvent(
-                                HoverEvent.Action.SHOW_TEXT, TranslationTextComponent(
-                                    "native.command.restricted_hover",
-                                    "native.clear", "2"
-                                )
+                            hoverEventFrom(
+                                source.asPlayer(),
+                                LocalizationConfigurationUtils.getConfig().enabled,
+                                "native.command.restricted_hover",
+                                "native.clear", "2"
                             )
                         )
                     )
