123cf0a7beca5d1454d1a6517198a755a680596b
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-03-11 20:27:15 +0300

    setblock command have got safe localization.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/vanilla/commands/SetBlockCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/vanilla/commands/SetBlockCommand.kt
@@ -7,6 +7,9 @@
 package com.mairwunnx.projectessentials.core.vanilla.commands
 
 import com.mairwunnx.projectessentials.core.EntryPoint
+import com.mairwunnx.projectessentials.core.configuration.localization.LocalizationConfigurationUtils
+import com.mairwunnx.projectessentials.core.extensions.hoverEventFrom
+import com.mairwunnx.projectessentials.core.extensions.textComponentFrom
 import com.mairwunnx.projectessentials.core.helpers.PERMISSION_LEVEL
 import com.mairwunnx.projectessentials.core.vanilla.utils.NativeCommandUtils
 import com.mojang.brigadier.CommandDispatcher
@@ -25,7 +28,6 @@ import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.MutableBoundingBox
 import net.minecraft.util.text.Style
 import net.minecraft.util.text.TranslationTextComponent
-import net.minecraft.util.text.event.HoverEvent
 import net.minecraft.world.server.ServerWorld
 import org.apache.logging.log4j.LogManager
 import java.util.function.Predicate
@@ -101,15 +103,17 @@ internal object SetBlockCommand {
                         .replace("%1", "setblock")
                 )
                 throw CommandException(
-                    TranslationTextComponent(
+                    textComponentFrom(
+                        source.asPlayer(),
+                        LocalizationConfigurationUtils.getConfig().enabled,
                         "native.command.restricted"
                     ).setStyle(
                         Style().setHoverEvent(
-                            HoverEvent(
-                                HoverEvent.Action.SHOW_TEXT, TranslationTextComponent(
-                                    "native.command.restricted_hover",
-                                    "native.setblock", "2"
-                                )
+                            hoverEventFrom(
+                                source.asPlayer(),
+                                LocalizationConfigurationUtils.getConfig().enabled,
+                                "native.command.restricted_hover",
+                                "native.setblock", "2"
                             )
                         )
                     )
