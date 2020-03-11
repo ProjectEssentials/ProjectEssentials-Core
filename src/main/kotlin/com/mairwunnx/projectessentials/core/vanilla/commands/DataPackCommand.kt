5c4e28c27ceb82e67c069ad4b3eb5ef17f410f87
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-03-11 20:02:59 +0300

    Datapack command have got safe localization.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/vanilla/commands/DataPackCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/vanilla/commands/DataPackCommand.kt
@@ -12,6 +12,9 @@ import com.google.common.collect.Lists
 import com.mairwunnx.projectessentials.cooldown.essentials.CommandsAliases
 import com.mairwunnx.projectessentials.core.EntryPoint
 import com.mairwunnx.projectessentials.core.configuration.commands.CommandsConfigurationUtils
+import com.mairwunnx.projectessentials.core.configuration.localization.LocalizationConfigurationUtils
+import com.mairwunnx.projectessentials.core.extensions.hoverEventFrom
+import com.mairwunnx.projectessentials.core.extensions.textComponentFrom
 import com.mairwunnx.projectessentials.core.helpers.PERMISSION_LEVEL
 import com.mairwunnx.projectessentials.core.vanilla.utils.NativeCommandUtils
 import com.mojang.brigadier.CommandDispatcher
@@ -28,7 +31,6 @@ import net.minecraft.resources.ResourcePackInfo
 import net.minecraft.util.text.Style
 import net.minecraft.util.text.TextComponentUtils
 import net.minecraft.util.text.TranslationTextComponent
-import net.minecraft.util.text.event.HoverEvent
 import net.minecraft.world.dimension.DimensionType
 import org.apache.logging.log4j.LogManager
 import java.util.function.Consumer
@@ -236,15 +238,17 @@ internal object DataPackCommand {
                         .replace("%1", "datapack")
                 )
                 throw CommandException(
-                    TranslationTextComponent(
+                    textComponentFrom(
+                        source.asPlayer(),
+                        LocalizationConfigurationUtils.getConfig().enabled,
                         "native.command.restricted"
                     ).setStyle(
                         Style().setHoverEvent(
-                            HoverEvent(
-                                HoverEvent.Action.SHOW_TEXT, TranslationTextComponent(
-                                    "native.command.restricted_hover",
-                                    "native.datapack", "2"
-                                )
+                            hoverEventFrom(
+                                source.asPlayer(),
+                                LocalizationConfigurationUtils.getConfig().enabled,
+                                "native.command.restricted_hover",
+                                "native.datapack", "2"
                             )
                         )
                     )
