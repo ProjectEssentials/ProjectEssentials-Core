f5cc2800e63694a753ff3d0ab00c202a421907f8
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-03-11 20:30:03 +0300

    summon command have got safe localization.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/vanilla/commands/SummonCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/vanilla/commands/SummonCommand.kt
@@ -9,6 +9,9 @@ package com.mairwunnx.projectessentials.core.vanilla.commands
 import com.mairwunnx.projectessentials.cooldown.essentials.CommandsAliases
 import com.mairwunnx.projectessentials.core.EntryPoint
 import com.mairwunnx.projectessentials.core.configuration.commands.CommandsConfigurationUtils
+import com.mairwunnx.projectessentials.core.configuration.localization.LocalizationConfigurationUtils
+import com.mairwunnx.projectessentials.core.extensions.hoverEventFrom
+import com.mairwunnx.projectessentials.core.extensions.textComponentFrom
 import com.mairwunnx.projectessentials.core.helpers.PERMISSION_LEVEL
 import com.mairwunnx.projectessentials.core.vanilla.utils.NativeCommandUtils
 import com.mojang.brigadier.CommandDispatcher
@@ -32,7 +35,6 @@ import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Vec3d
 import net.minecraft.util.text.Style
 import net.minecraft.util.text.TranslationTextComponent
-import net.minecraft.util.text.event.HoverEvent
 import org.apache.logging.log4j.LogManager
 
 internal object SummonCommand {
@@ -111,15 +113,17 @@ internal object SummonCommand {
                         .replace("%1", "summon")
                 )
                 throw CommandException(
-                    TranslationTextComponent(
+                    textComponentFrom(
+                        source.asPlayer(),
+                        LocalizationConfigurationUtils.getConfig().enabled,
                         "native.command.restricted"
                     ).setStyle(
                         Style().setHoverEvent(
-                            HoverEvent(
-                                HoverEvent.Action.SHOW_TEXT, TranslationTextComponent(
-                                    "native.command.restricted_hover",
-                                    "native.summon", "2"
-                                )
+                            hoverEventFrom(
+                                source.asPlayer(),
+                                LocalizationConfigurationUtils.getConfig().enabled,
+                                "native.command.restricted_hover",
+                                "native.summon", "2"
                             )
                         )
                     )
