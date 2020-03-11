177db5867a482063cbe3ea942993da682971f0fe
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-03-11 20:27:42 +0300

    setidletimeout command have got safe localization.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/vanilla/commands/SetIdleTimeoutCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/vanilla/commands/SetIdleTimeoutCommand.kt
@@ -7,6 +7,9 @@
 package com.mairwunnx.projectessentials.core.vanilla.commands
 
 import com.mairwunnx.projectessentials.core.EntryPoint
+import com.mairwunnx.projectessentials.core.configuration.localization.LocalizationConfigurationUtils
+import com.mairwunnx.projectessentials.core.extensions.hoverEventFrom
+import com.mairwunnx.projectessentials.core.extensions.textComponentFrom
 import com.mairwunnx.projectessentials.core.helpers.PERMISSION_LEVEL
 import com.mairwunnx.projectessentials.core.vanilla.utils.NativeCommandUtils
 import com.mojang.brigadier.CommandDispatcher
@@ -17,7 +20,6 @@ import net.minecraft.command.CommandSource
 import net.minecraft.command.Commands
 import net.minecraft.util.text.Style
 import net.minecraft.util.text.TranslationTextComponent
-import net.minecraft.util.text.event.HoverEvent
 import org.apache.logging.log4j.LogManager
 
 internal object SetIdleTimeoutCommand {
@@ -51,15 +53,17 @@ internal object SetIdleTimeoutCommand {
                         .replace("%1", "setidletimeout")
                 )
                 throw CommandException(
-                    TranslationTextComponent(
+                    textComponentFrom(
+                        source.asPlayer(),
+                        LocalizationConfigurationUtils.getConfig().enabled,
                         "native.command.restricted"
                     ).setStyle(
                         Style().setHoverEvent(
-                            HoverEvent(
-                                HoverEvent.Action.SHOW_TEXT, TranslationTextComponent(
-                                    "native.command.restricted_hover",
-                                    "native.setidletimeout", "3"
-                                )
+                            hoverEventFrom(
+                                source.asPlayer(),
+                                LocalizationConfigurationUtils.getConfig().enabled,
+                                "native.command.restricted_hover",
+                                "native.setidletimeout", "3"
                             )
                         )
                     )
