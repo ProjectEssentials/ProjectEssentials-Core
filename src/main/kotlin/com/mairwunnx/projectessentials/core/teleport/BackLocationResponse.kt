469a6fa2f067cf23e24822790d6b995a95f492e6
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-03-11 21:37:12 +0300

    Back location provider implemented.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

---
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/teleport/BackLocationResponse.kt
@@ -0,0 +1,7 @@
+package com.mairwunnx.projectessentials.core.teleport
+
+enum class BackLocationResponse {
+    SUCCESS,
+    ALREADY_REVOKED,
+    EXPIRED // todo: implement it, if community it need.
+}
