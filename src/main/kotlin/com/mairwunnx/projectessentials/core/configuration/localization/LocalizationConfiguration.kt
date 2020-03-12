763bd545f440124ae346183506a13a944c369ae6
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-03-12 23:09:41 +0300

    LocalizationConfiguration now is public. LocalizationConfigurationUtils now is public.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/configuration/localization/LocalizationConfiguration.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/configuration/localization/LocalizationConfiguration.kt
@@ -3,7 +3,7 @@ package com.mairwunnx.projectessentials.core.configuration.localization
 import kotlinx.serialization.Serializable
 
 @Serializable
-internal data class LocalizationConfiguration(
+data class LocalizationConfiguration(
     val enabled: Boolean = false,
     val fallbackLanguage: String = "en_us"
 )
