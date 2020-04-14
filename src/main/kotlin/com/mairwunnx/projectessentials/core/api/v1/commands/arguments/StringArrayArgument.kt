a9286f335bb00cef160c0675cddd0ece7b72a663
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-04-14 12:10:10 +0300

    `StringArrayArgument` public documentation added. Exception message in `StringArrayArgument#parse` changed.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/api/v1/commands/arguments/StringArrayArgument.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/api/v1/commands/arguments/StringArrayArgument.kt
@@ -9,13 +9,19 @@ import net.minecraft.command.CommandSource
 import net.minecraft.command.ISuggestionProvider
 import java.util.concurrent.CompletableFuture
 
+/**
+ * String argument type class, for commands.
+ *
+ * @param array array with accepted values for command.
+ * @since Mod: 2.0.0-RC.1+MC-1.14.4, API: 1.0.0
+ */
 class StringArrayArgument(private val array: List<String>) : ArgumentType<String> {
     override fun parse(reader: StringReader): String {
         val value = reader.readUnquotedString()
         return if (value in array) {
             array.find { it == value } ?: value
         } else {
-            throw IllegalArgumentException("No settings found for name ${value}.")
+            throw IllegalArgumentException("No value found for name $value in array.")
         }
     }
 
@@ -25,9 +31,23 @@ class StringArrayArgument(private val array: List<String>) : ArgumentType<String
     ): CompletableFuture<Suggestions> = ISuggestionProvider.suggest(array.sorted(), builder)
 
     companion object {
+        /**
+         * Creates new instance of [StringArrayArgument].
+         *
+         * @param array array with accepted value for command.
+         * @return new instance of [StringArrayArgument] for command.
+         * @since Mod: 2.0.0-RC.1+MC-1.14.4, API: 1.0.0
+         */
         @JvmStatic
         fun with(array: List<String>) = StringArrayArgument(array)
 
+        /**
+         * @param context command context with parameter type [CommandSource].
+         * @param name argument name what contains your argument value.
+         * @throws IllegalArgumentException when argument not exist in command context.
+         * @return selected value by player in command.
+         * @since Mod: 2.0.0-RC.1+MC-1.14.4, API: 1.0.0
+         */
         @JvmStatic
         fun getValue(
             context: CommandContext<CommandSource>, name: String
