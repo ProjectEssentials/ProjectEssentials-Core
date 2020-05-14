d31e9c2b670ce04ee76890870541fba043a005df
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-14 13:38:15 +0300

    Default permission resolution strategy improved.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/api/v1/permissions/strategy/DefaultPermissionResolutionStrategy.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/api/v1/permissions/strategy/DefaultPermissionResolutionStrategy.kt
@@ -1,6 +1,7 @@
 package com.mairwunnx.projectessentials.core.api.v1.permissions.strategy
 
 import net.minecraft.entity.player.ServerPlayerEntity
+import net.minecraftforge.server.permission.DefaultPermissionHandler
 import net.minecraftforge.server.permission.PermissionAPI
 
 /**
@@ -15,8 +16,12 @@ import net.minecraftforge.server.permission.PermissionAPI
 class DefaultPermissionResolutionStrategy : IPermissionResolutionStrategy {
     override fun hasPermission(
         player: ServerPlayerEntity, node: String, opLevel: Int
-    ) = when {
-        PermissionAPI.hasPermission(player, node) -> true
-        else -> player.hasPermissionLevel(opLevel)
+    ) = if (PermissionAPI.getPermissionHandler() is DefaultPermissionHandler) {
+        when {
+            PermissionAPI.hasPermission(player, node) -> true
+            else -> player.hasPermissionLevel(opLevel)
+        }
+    } else {
+        PermissionAPI.hasPermission(player, node)
     }
 }
