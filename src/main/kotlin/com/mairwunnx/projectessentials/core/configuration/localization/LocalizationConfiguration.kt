56ee228503c62bc0cfa4bc3d1ec13bb25a0d9a70
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-16 20:27:40 +0300

    Removed old files after merge 1.14.4 branch.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/configuration/localization/LocalizationConfiguration.kt
+++
@@ -1,9 +0,0 @@
-package com.mairwunnx.projectessentials.core.configuration.localization
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class LocalizationConfiguration(
-    val enabled: Boolean = false,
-    val fallbackLanguage: String = "en_us"
-)
