f06f521d4d64eb9e875d8a56506d311438c3c36a
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-05-31 20:39:12 +0300

    Auto-complete general settings implemented.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/impl/commands/CommandLiterals.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/impl/commands/CommandLiterals.kt
@@ -1,25 +1,28 @@
 package com.mairwunnx.projectessentials.core.impl.commands
 
-import com.mairwunnx.projectessentials.core.api.v1.commands.arguments.StringArrayArgument
-import com.mairwunnx.projectessentials.core.api.v1.configuration.ConfigurationAPI
+import com.mairwunnx.projectessentials.core.api.v1.configuration.ConfigurationAPI.getConfigurationByName
 import com.mairwunnx.projectessentials.core.impl.configurations.GeneralConfiguration
 import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
-import com.mojang.brigadier.builder.LiteralArgumentBuilder.literal
 import net.minecraft.command.CommandSource
 import net.minecraft.command.Commands
+import net.minecraft.command.ISuggestionProvider
 
-fun takeConfigureEssentialsLiteral(): LiteralArgumentBuilder<CommandSource> =
-    literal<CommandSource>("configure-essentials").then(
-        Commands.argument("setting", StringArrayArgument.with(
-            ConfigurationAPI.getConfigurationByName<GeneralConfiguration>(
-                "general"
-            ).take().keys.map { it.toString() }
-        )).then(
+inline val configureEssentialsLiteral: LiteralArgumentBuilder<CommandSource>
+    get() = Commands.literal("configure-essentials").then(
+        Commands.argument(
+            "setting", StringArgumentType.string()
+        ).suggests { _, builder ->
+            ISuggestionProvider.suggest(
+                getConfigurationByName<GeneralConfiguration>(
+                    "general"
+                ).take().keys.map { it as String }, builder
+            )
+        }.then(
             Commands.literal("set").then(
-                Commands.argument("value", StringArgumentType.string()).executes(
-                    ConfigureEssentialsCommand::process
-                )
+                Commands.argument("value", StringArgumentType.string()).executes {
+                    ConfigureEssentialsCommand.process(it)
+                }
             )
         )
     )
