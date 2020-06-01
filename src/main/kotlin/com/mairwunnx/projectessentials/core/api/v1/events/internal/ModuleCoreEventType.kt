3e4e4d417eef9ce8105000da0b362e0e9df374a7
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-01 18:38:25 +0300

    ModuleCoreEventType.kt cleanup.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/api/v1/events/internal/ModuleCoreEventType.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/api/v1/events/internal/ModuleCoreEventType.kt
@@ -9,62 +9,6 @@ import com.mairwunnx.projectessentials.core.api.v1.events.IModuleEventType
 enum class ModuleCoreEventType : IModuleEventType {
     /**
      * This event type will be fired on
-     * processor registering.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    OnProcessorRegister,
-
-    /**
-     * This event type will be fired after
-     * processor registering.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    OnProcessorAfterRegister,
-
-    /**
-     * This event type will be fired on
-     * processor initializing.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    OnProcessorInitializing,
-
-    /**
-     * This event type will be fired after
-     * processor initializing.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    OnProcessorAfterInitializing,
-
-    /**
-     * This event type will be fired on
-     * processor processing.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    OnProcessorProcessing,
-
-    /**
-     * This event type will be fired after
-     * processor processing.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    OnProcessorAfterProcessing,
-
-    /**
-     * This event type will be fired on
-     * processor post processing.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    OnProcessorPostProcessing,
-
-    /**
-     * This event type will be fired after
-     * processor post processing.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    OnProcessorAfterPostProcessing,
-
-    /**
-     * This event type will be fired on
      * module class processing.
      * @since 2.0.0-SNAPSHOT.1.
      */
@@ -76,13 +20,6 @@ enum class ModuleCoreEventType : IModuleEventType {
      * @since 2.0.0-SNAPSHOT.1.
      */
     OnModuleClassProcessed,
-
-    /**
-     * This event type will be fired on
-     * module class post processing.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    OnModuleClassPostProcessing,
 
     /**
      * This event type will be fired on
@@ -100,34 +37,6 @@ enum class ModuleCoreEventType : IModuleEventType {
 
     /**
      * This event type will be fired on
-     * configuration class post processing.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    OnConfigurationClassPostProcessing,
-
-    /**
-     * This event type will be fired on
-     * localization initializing.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    OnLocalizationInitializing,
-
-    /**
-     * This event type will be fired on
-     * localization processing.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    OnLocalizationProcessing,
-
-    /**
-     * This event type will be fired after
-     * localization processing.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    OnLocalizationProcessed,
-
-    /**
-     * This event type will be fired on
      * command class processing.
      * @since 2.0.0-SNAPSHOT.1.
      */
@@ -138,12 +47,5 @@ enum class ModuleCoreEventType : IModuleEventType {
      * command class processing.
      * @since 2.0.0-SNAPSHOT.1.
      */
-    OnCommandClassProcessed,
-
-    /**
-     * This event type will be fired on
-     * command class post processing.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    OnCommandClassPostProcessing
+    OnCommandClassProcessed
 }
