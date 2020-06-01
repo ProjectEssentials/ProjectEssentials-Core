483405ccf79b03f5dd9fb55aaeddda7d579f168c
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-01 18:38:11 +0300

    ProviderType.kt naming changed.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/api/v1/providers/ProviderType.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/api/v1/providers/ProviderType.kt
@@ -4,22 +4,4 @@ package com.mairwunnx.projectessentials.core.api.v1.providers
  * Enum class of provider types.
  * @since 2.0.0-SNAPSHOT.1.
  */
-enum class ProviderType {
-    /**
-     * Configuration provider type.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    CONFIGURATION,
-
-    /**
-     * Module provider type.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    MODULE,
-
-    /**
-     * Command provider type.
-     * @since 2.0.0-SNAPSHOT.1.
-     */
-    COMMAND
-}
+enum class ProviderType { Configuration, Module, Command }
