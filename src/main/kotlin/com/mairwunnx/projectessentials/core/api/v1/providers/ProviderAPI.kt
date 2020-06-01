9e1175756151ba58413d048f3a3fa56b453917af
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-01 18:34:51 +0300

    ProviderAPI.kt improved.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/api/v1/providers/ProviderAPI.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/api/v1/providers/ProviderAPI.kt
@@ -1,75 +1,46 @@
+@file:Suppress("MemberVisibilityCanBePrivate")
+
 package com.mairwunnx.projectessentials.core.api.v1.providers
 
 import com.mairwunnx.projectessentials.core.api.v1.commands.ICommand
 import com.mairwunnx.projectessentials.core.api.v1.configuration.IConfiguration
 import com.mairwunnx.projectessentials.core.api.v1.module.IModule
+import com.mairwunnx.projectessentials.core.api.v1.providersMarker
 import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.MarkerManager
 
 /**
  * Provider API class. If you build new module
  * for project essentials then you must use this class.
  *
- * You can register providers (configurations, commands and module).
- *
- * You can register manually (just addProvider(<provider class>)) and you can
- * use ClassIndex library, code example (kotlin language) (it scans all
- * configuration classes with annotations):
- *
- * ```kotlin
- * ClassIndex.getAnnotated(Configuration::class.java).forEach {
- *   ProviderAPI.addProvider(it)
- * }
- * ```
- *
- * BTW! `addProvider(...)` you must call only in init block of your mod.
- * (or if you use java as language then in constructor)
- *
- * ["ClassIndex library"](https://github.com/atteo/classindex)
- *
  * @since 2.0.0-SNAPSHOT.1.
  */
 object ProviderAPI {
     private val logger = LogManager.getLogger()
-    private val marker = MarkerManager.Log4jMarker("PROVIDER")
     private val providers = mutableMapOf<ProviderType, MutableList<Class<*>>>()
 
     /**
      * Adds target provider. (provider type will determine automatically)
      *
-     * **Call only in module initialize block or constructor!!**
      * @param clazz provider class.
      * @since 2.0.0-SNAPSHOT.1.
      */
-    @Synchronized
     fun addProvider(clazz: Class<*>) {
+        fun out(type: String) = logger.debug(
+            providersMarker, "Provider class found: Type: `$type`, Class: `${clazz.simpleName}`"
+        )
+
         when {
             IConfiguration::class.java.isAssignableFrom(clazz) -> {
-                addProvider(ProviderType.CONFIGURATION, clazz).run {
-                    logger.debug(
-                        marker,
-                        "Provider class founded: Type: `Configuration`, Class: `${clazz.simpleName}`"
-                    )
-                }
+                addProvider(ProviderType.Configuration, clazz).run { out("Configuration") }
             }
             IModule::class.java.isAssignableFrom(clazz) -> {
-                addProvider(ProviderType.MODULE, clazz).run {
-                    logger.debug(
-                        marker,
-                        "Provider class founded: Type: `Module`, Class: `${clazz.simpleName}`"
-                    )
-                }
+                addProvider(ProviderType.Module, clazz).run { out("Module") }
             }
             ICommand::class.java.isAssignableFrom(clazz) -> {
-                addProvider(ProviderType.COMMAND, clazz).run {
-                    logger.debug(
-                        marker,
-                        "Provider class founded: Type: `Command`, Class: `${clazz.simpleName}`"
-                    )
-                }
+                addProvider(ProviderType.Command, clazz).run { out("Command") }
             }
             else -> logger.warn(
-                marker,
+                providersMarker,
                 "Incorrect provider class found! (skipped to load): Class: `${clazz.simpleName}`"
             )
         }
@@ -78,7 +49,6 @@ object ProviderAPI {
     /**
      * Adds target provider with specified type.
      *
-     * **Call only in module initialize block or constructor!!**
      * @param type provider type.
      * @param clazz provider class.
      * @since 2.0.0-SNAPSHOT.1.
@@ -91,12 +61,12 @@ object ProviderAPI {
      * @return all added providers.
      * @since 2.0.0-SNAPSHOT.1.
      */
-    fun getAllProviders() = providers
+    fun getProviders() = providers
 
     /**
      * @param type provider type to return.
      * @return all specified for type providers.
      * @since 2.0.0-SNAPSHOT.1.
      */
-    fun getProvidersByType(type: ProviderType) = getAllProviders()[type] ?: mutableListOf()
+    fun getProvidersByType(type: ProviderType) = getProviders()[type] ?: mutableListOf()
 }
