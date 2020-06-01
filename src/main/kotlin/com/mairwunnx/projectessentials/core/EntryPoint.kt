820b9f81b110f5b17855482644d8366a8c347112
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-01 16:39:00 +0300

    Processing localization messaging added.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/EntryPoint.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/EntryPoint.kt
@@ -1,5 +1,6 @@
 package com.mairwunnx.projectessentials.core
 
+import com.mairwunnx.projectessentials.core.api.v1.IMCLocalizationMessage
 import com.mairwunnx.projectessentials.core.api.v1.commands.CommandProcessor
 import com.mairwunnx.projectessentials.core.api.v1.configuration.ConfigurationProcessor
 import com.mairwunnx.projectessentials.core.api.v1.events.ModuleEventAPI.subscribeOn
@@ -7,9 +8,8 @@ import com.mairwunnx.projectessentials.core.api.v1.events.forge.FMLCommonSetupEv
 import com.mairwunnx.projectessentials.core.api.v1.events.forge.ForgeEventType
 import com.mairwunnx.projectessentials.core.api.v1.events.forge.InterModEnqueueEventData
 import com.mairwunnx.projectessentials.core.api.v1.events.forge.InterModProcessEventData
-import com.mairwunnx.projectessentials.core.api.v1.localization.Localization
 import com.mairwunnx.projectessentials.core.api.v1.localization.LocalizationAPI
-import com.mairwunnx.projectessentials.core.api.v1.localization.LocalizationProcessor
+import com.mairwunnx.projectessentials.core.api.v1.localizationMarker
 import com.mairwunnx.projectessentials.core.api.v1.module.ModuleProcessor
 import com.mairwunnx.projectessentials.core.api.v1.processor.ProcessorAPI
 import com.mairwunnx.projectessentials.core.api.v1.providers.ProviderAPI
@@ -20,22 +20,16 @@ import com.mairwunnx.projectessentials.core.impl.configurations.GeneralConfigura
 import com.mairwunnx.projectessentials.core.impl.configurations.NativeAliasesConfiguration
 import com.mairwunnx.projectessentials.core.impl.events.EventBridge
 import net.minecraftforge.common.MinecraftForge.EVENT_BUS
+import net.minecraftforge.fml.InterModComms
+import net.minecraftforge.fml.ModList
 import net.minecraftforge.fml.common.Mod
+import org.apache.logging.log4j.LogManager
 
 @Suppress("unused")
 @Mod("project_essentials_core")
 internal class EntryPoint {
-    /*
-        Sorry for hardcoded classes in this list.
-        I tried getting this classes with library https://github.com/matfax/klassindex
-        but it incorrectly works with multi-module projects,
-        for example if core will using klassindex library, then
-        other modules will incorrectly finding annotated classes,
-        because same class path of generated object with class references.
+    private val logger = LogManager.getLogger()
 
-        If you know how resolve it, please, open issue or pull request
-        and we can discuss it.
-     */
     private val providers = listOf(
         GeneralConfiguration::class.java,
         NativeAliasesConfiguration::class.java,
@@ -47,47 +41,63 @@ internal class EntryPoint {
     private val processors = listOf(
         ConfigurationProcessor,
         ModuleProcessor,
-        LocalizationProcessor,
         CommandProcessor
     )
 
     init {
         EventBridge.initialize()
-        providers.forEach(ProviderAPI::addProvider)
-        subscribeEvents()
         EVENT_BUS.register(this)
+        subscribeEvents()
+        providers.forEach(ProviderAPI::addProvider)
     }
 
     private fun subscribeEvents() {
         subscribeOn<FMLCommonSetupEventData>(
             ForgeEventType.SetupEvent
         ) {
-            applyLocalization()
             processors.forEach(ProcessorAPI::register)
         }
 
         subscribeOn<InterModEnqueueEventData>(
             ForgeEventType.EnqueueIMCEvent
         ) {
+            sendLocalizationRequest()
             ProcessorAPI.processProcessors()
         }
 
         subscribeOn<InterModProcessEventData>(
             ForgeEventType.ProcessIMCEvent
-        ) {
+        ) { event ->
+            processLocalizationRequest(event)
             ProcessorAPI.postProcessProcessors()
         }
     }
 
-    private fun applyLocalization() {
-        LocalizationAPI.apply(
-            Localization(
-                mutableListOf(
-                    "/assets/projectessentialscore/lang/en_us.json",
-                    "/assets/projectessentialscore/lang/ru_ru.json",
-                    "/assets/projectessentialscore/lang/zh_cn.json"
-                ), "core", EntryPoint::class.java
+    private fun sendLocalizationRequest() {
+        InterModComms.sendTo(
+            "project_essentials_core",
+            IMCLocalizationMessage
+        ) {
+            fun() = mutableListOf(
+                "/assets/projectessentialscore/lang/en_us.json",
+                "/assets/projectessentialscore/lang/ru_ru.json",
+                "/assets/projectessentialscore/lang/zh_cn.json"
             )
-        )
+        }
+    }
+
+    private fun processLocalizationRequest(event: InterModProcessEventData) {
+        event.event.getIMCStream { method ->
+            method == IMCLocalizationMessage
+        }.also { stream ->
+            stream.forEach { message ->
+                val clazz = ModList.get().getModContainerById(message.modId).get().mod.javaClass
+                message.getMessageSupplier<() -> List<String>>().get().also {
+                    logger.debug(
+                        localizationMarker, "Localization got from ${message.senderModId}"
+                    ).run { LocalizationAPI.apply(clazz, it) }
+                }
+            }
+        }
     }
 }
