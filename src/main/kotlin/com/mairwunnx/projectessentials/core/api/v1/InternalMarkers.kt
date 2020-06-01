0627a12993727c7dfb1f8c09eb49ad20d59d8af2
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-01 18:37:47 +0300

    `providerMarker` added to `InternalMarkers.kt`.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/api/v1/InternalMarkers.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/api/v1/InternalMarkers.kt
@@ -3,3 +3,4 @@ package com.mairwunnx.projectessentials.core.api.v1
 import org.apache.logging.log4j.MarkerManager
 
 val localizationMarker = MarkerManager.getMarker("L10N")
+val providersMarker = MarkerManager.getMarker("PROVIDERS")
