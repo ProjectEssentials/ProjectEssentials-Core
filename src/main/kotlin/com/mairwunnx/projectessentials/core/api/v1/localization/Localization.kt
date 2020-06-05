f7c09fc44009d2dc32533925e8773d0ec5b23e05
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-05 15:07:35 +0300

    Deprecated annotation removed for Localization data class.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/api/v1/localization/Localization.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/api/v1/localization/Localization.kt
@@ -1,6 +1,5 @@
 package com.mairwunnx.projectessentials.core.api.v1.localization
 
-@Deprecated("Deprecated feature, but still existing for backward compatibility.")
 data class Localization(
     val sources: MutableList<String>, val sourceName: String, val sourceClass: Class<*>
 )
