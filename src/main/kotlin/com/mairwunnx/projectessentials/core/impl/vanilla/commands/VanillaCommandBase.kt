3db82ffaccbdf54bfe18178ef79d8b8dfd56ba07
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-10 19:36:02 +0300

    logger remvoed from VanillaCommandBase.kt.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/impl/vanilla/commands/VanillaCommandBase.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/impl/vanilla/commands/VanillaCommandBase.kt
@@ -3,11 +3,8 @@ package com.mairwunnx.projectessentials.core.impl.vanilla.commands
 import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAPI
 import com.mojang.brigadier.CommandDispatcher
 import net.minecraft.command.CommandSource
-import org.apache.logging.log4j.LogManager
-import org.apache.logging.log4j.Logger
 
 abstract class VanillaCommandBase(val name: String) {
-    val logger: Logger = LogManager.getLogger()
     open fun register(dispatcher: CommandDispatcher<CommandSource>) {
         CommandAPI.removeCommand(name)
     }
