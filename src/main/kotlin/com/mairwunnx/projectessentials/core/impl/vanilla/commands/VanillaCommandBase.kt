7041216070fa3a7aa0c0fef7302240628815af76
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-10 18:57:25 +0300

    VanillaCommandBase.kt simplified.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/impl/vanilla/commands/VanillaCommandBase.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/impl/vanilla/commands/VanillaCommandBase.kt
@@ -1,13 +1,14 @@
 package com.mairwunnx.projectessentials.core.impl.vanilla.commands
 
-import com.mairwunnx.projectessentials.core.api.v1.configuration.ConfigurationAPI.getConfigurationByName
-import com.mairwunnx.projectessentials.core.impl.configurations.GeneralConfiguration
-import com.mairwunnx.projectessentials.core.impl.configurations.NativeAliasesConfiguration
+import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAPI
+import com.mojang.brigadier.CommandDispatcher
+import net.minecraft.command.CommandSource
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
-abstract class VanillaCommandBase {
+abstract class VanillaCommandBase(val name: String) {
     val logger: Logger = LogManager.getLogger()
-    val generalConfiguration = getConfigurationByName<GeneralConfiguration>("general")
-    val configuration = getConfigurationByName<NativeAliasesConfiguration>("native-aliases")
+    open fun register(dispatcher: CommandDispatcher<CommandSource>) {
+        CommandAPI.removeCommand(name)
+    }
 }
