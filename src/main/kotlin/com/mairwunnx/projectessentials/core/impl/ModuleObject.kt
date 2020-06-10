539a52e43eebb2687c951c5e997b60b10870f7b1
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-10 18:57:10 +0300

    Removed all native commands from registerNativeCommands method.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/impl/ModuleObject.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/impl/ModuleObject.kt
@@ -12,10 +12,13 @@ import com.mairwunnx.projectessentials.core.api.v1.extensions.isPlayerEntity
 import com.mairwunnx.projectessentials.core.api.v1.helpers.projectConfigDirectory
 import com.mairwunnx.projectessentials.core.api.v1.messaging.MessagingAPI
 import com.mairwunnx.projectessentials.core.api.v1.module.IModule
+import com.mairwunnx.projectessentials.core.api.v1.module.ModuleAPI
 import com.mairwunnx.projectessentials.core.api.v1.permissions.hasPermission
 import com.mairwunnx.projectessentials.core.impl.commands.ConfigureEssentialsCommandAPI
 import com.mairwunnx.projectessentials.core.impl.configurations.GeneralConfiguration
-import com.mairwunnx.projectessentials.core.impl.vanilla.commands.*
+import com.mairwunnx.projectessentials.core.impl.utils.NativeCommandUtils.insertNativeAliases
+import com.mairwunnx.projectessentials.core.impl.utils.NativeCommandUtils.replaceRequirementPredicates
+import com.mairwunnx.projectessentials.core.impl.vanilla.commands.EnchantCommand
 import com.mojang.brigadier.CommandDispatcher
 import net.minecraft.command.CommandSource
 import net.minecraft.entity.player.ServerPlayerEntity
@@ -30,6 +33,7 @@ import net.minecraftforge.fml.event.server.FMLServerStartingEvent
 import net.minecraftforge.fml.event.server.FMLServerStoppingEvent
 import org.apache.logging.log4j.LogManager
 import java.io.File
+
 
 internal class ModuleObject : IModule {
     override val name = this::class.java.`package`.implementationTitle.split(" ").last()
@@ -125,16 +129,21 @@ Project Essentials §c§ois not a commercial project §fand all its modules dist
         generalConfiguration.getBoolOrDefault(SETTING_LOC_ENABLED, false)
         generalConfiguration.getStringOrDefault(SETTING_LOC_FALLBACK_LANG, "en_us")
         generalConfiguration.getBoolOrDefault(SETTING_DISABLE_SAFE_ENCHANT, false)
-        generalConfiguration.getBoolOrDefault(SETTING_NATIVE_COMMAND_REPLACE, true)
+        generalConfiguration.getBoolOrDefault(SETTING_NATIVE_COMMAND_ALIASES_REPLACE, true)
         generalConfiguration.getIntOrDefault(SETTING_LOCATE_COMMAND_FIND_RADIUS, 100)
         generalConfiguration.getBoolOrDefault(SETTING_DISABLE_PORTAL_SPAWNING, false)
         generalConfiguration.getIntOrDefault(SETTING_WEATHER_COMMAND_DEFAULT_DURATION, 6000)
         generalConfiguration.getBoolOrDefault(SETTING_DEOP_COMMAND_REMOVE_OP_PERM, true)
         generalConfiguration.getIntOrDefault(SETTING_LIST_MAX_ELEMENTS_IN_PAGE, 8)
         generalConfiguration.getBoolOrDefault(SETTING_ENABLE_CONSOLE_COLORS, false)
+        generalConfiguration.getBoolOrDefault(SETTING_NATIVE_COMMAND_REPLACE, true)
+        generalConfiguration.getBoolOrDefault(
+            SETTING_NATIVE_COMMAND_REQUIRE_PREDICATE_REPLACE, true
+        )
 
         ConfigureEssentialsCommandAPI.required(SETTING_NATIVE_COMMAND_REPLACE)
-        ConfigureEssentialsCommandAPI.required(SETTING_LOC_FALLBACK_LANG)
+        ConfigureEssentialsCommandAPI.required(SETTING_NATIVE_COMMAND_REQUIRE_PREDICATE_REPLACE)
+        ConfigureEssentialsCommandAPI.required(SETTING_NATIVE_COMMAND_ALIASES_REPLACE)
     }
 
     private fun printGreetingMessage() = LogManager.getLogger().warn(
@@ -156,77 +165,7 @@ Thanks for using my project! </3
         """
     )
 
-    private fun registerNativeCommands(
-        dispatcher: CommandDispatcher<CommandSource>,
-        isDedicatedServer: Boolean
-    ) {
-        AdvancementCommand.register(dispatcher)
-        BossBarCommand.register(dispatcher)
-        ClearCommand.register(dispatcher)
-        CloneCommand.register(dispatcher)
-        DataPackCommand.register(dispatcher)
-        DebugCommand.register(dispatcher)
-        DefaultGameModeCommand.register(dispatcher)
-        DifficultyCommand.register(dispatcher)
-        EffectCommand.register(dispatcher)
+    private fun registerNativeCommands(dispatcher: CommandDispatcher<CommandSource>) {
         EnchantCommand.register(dispatcher)
-        ExecuteCommand.register(dispatcher)
-        ExperienceCommand.register(dispatcher)
-        FillCommand.register(dispatcher)
-        ForceLoadCommand.register(dispatcher)
-        FunctionCommand.register(dispatcher)
-        GameModeCommand.register(dispatcher)
-        GameRuleCommand.register(dispatcher)
-        GiveCommand.register(dispatcher)
-        HelpCommand.register(dispatcher)
-        KickCommand.register(dispatcher)
-        KillCommand.register(dispatcher)
-        ListCommand.register(dispatcher)
-        LocateCommand.register(dispatcher)
-        LootCommand.register(dispatcher)
-        MeCommand.register(dispatcher)
-        MessageCommand.register(dispatcher)
-        ParticleCommand.register(dispatcher)
-        PlaySoundCommand.register(dispatcher)
-        PublishCommand.register(dispatcher)
-        RecipeCommand.register(dispatcher)
-        ReloadCommand.register(dispatcher)
-        ReplaceItemCommand.register(dispatcher)
-        SayCommand.register(dispatcher)
-        ScheduleCommand.register(dispatcher)
-        ScoreboardCommand.register(dispatcher)
-        SeedCommand.register(dispatcher)
-        SetBlockCommand.register(dispatcher)
-        SetWorldSpawnCommand.register(dispatcher)
-        SpawnPointCommand.register(dispatcher)
-        SpreadPlayersCommand.register(dispatcher)
-        StopSoundCommand.register(dispatcher)
-        SummonCommand.register(dispatcher)
-        TagCommand.register(dispatcher)
-        TeamCommand.register(dispatcher)
-        TeamMsgCommand.register(dispatcher)
-        TeleportCommand.register(dispatcher)
-        TellRawCommand.register(dispatcher)
-        TimeCommand.register(dispatcher)
-        TitleCommand.register(dispatcher)
-        TriggerCommand.register(dispatcher)
-        WeatherCommand.register(dispatcher)
-        WorldBorderCommand.register(dispatcher)
-
-        if (isDedicatedServer) {
-            BanCommand.register(dispatcher)
-            BanIpCommand.register(dispatcher)
-            BanListCommand.register(dispatcher)
-            DeOpCommand.register(dispatcher)
-            OpCommand.register(dispatcher)
-            PardonCommand.register(dispatcher)
-            PardonIpCommand.register(dispatcher)
-            SaveAllCommand.register(dispatcher)
-            SaveOffCommand.register(dispatcher)
-            SaveOnCommand.register(dispatcher)
-            SetIdleTimeoutCommand.register(dispatcher)
-            StopCommand.register(dispatcher)
-            WhitelistCommand.register(dispatcher)
-        }
     }
 }
