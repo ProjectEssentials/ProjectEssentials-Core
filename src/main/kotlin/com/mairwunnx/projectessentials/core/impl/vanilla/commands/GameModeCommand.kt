a6cfb44e2def511a3b9125fb18274ac2ab86ab85
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-10 19:26:26 +0300

    GameModeCommand.kt re-written.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/impl/vanilla/commands/GameModeCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/impl/vanilla/commands/GameModeCommand.kt
@@ -1,16 +1,13 @@
 /**
- * This command implementation by Mojang.
- * And decompiled with idea source code was converted
- * to kotlin code.
- * Also added some logic, for example checking on
- * permissions, and for some commands shorten aliases.
+ * ! This command implementation by Mojang Game Studios!
+ *
+ * Decompiled with idea source code was converted to kotlin code.
+ * But with additions such as permissions checking and etc.
  *
  * 1. This can be bad code.
  * 2. This file can be not formatter pretty.
  */
-
 package com.mairwunnx.projectessentials.core.impl.vanilla.commands
-
 
 import com.mairwunnx.projectessentials.core.api.v1.SETTING_LOC_ENABLED
 import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAPI
@@ -18,146 +15,87 @@ import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAliases
 import com.mairwunnx.projectessentials.core.api.v1.extensions.hoverEventFrom
 import com.mairwunnx.projectessentials.core.api.v1.extensions.textComponentFrom
 import com.mairwunnx.projectessentials.core.api.v1.permissions.hasPermission
+import com.mairwunnx.projectessentials.core.impl.generalConfiguration
 import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.context.CommandContext
-import com.mojang.brigadier.exceptions.CommandSyntaxException
 import net.minecraft.command.CommandException
 import net.minecraft.command.CommandSource
 import net.minecraft.command.Commands
 import net.minecraft.command.arguments.EntityArgument
+import net.minecraft.command.impl.GameModeCommand
 import net.minecraft.entity.player.ServerPlayerEntity
 import net.minecraft.util.text.Style
-import net.minecraft.util.text.TranslationTextComponent
-import net.minecraft.world.GameRules
 import net.minecraft.world.GameType
 
-internal object GameModeCommand : VanillaCommandBase() {
-    private var aliases = configuration.take().aliases.gamemode + "gamemode"
-
-    private fun tryAssignAliases() {
-        CommandAliases.aliases["gamemode"] = aliases.toMutableList()
-        CommandAliases.aliases["gamemode"]?.addAll(listOf("gmc", "gms", "gma", "gmsp"))
-    }
-
-    private fun registerShortAliases(dispatcher: CommandDispatcher<CommandSource>) {
-        Commands.literal("gmc").executes {
-            setGameMode(it, setOf(it.source.asPlayer()), GameType.CREATIVE)
-        }.then(
-            Commands.argument("target", EntityArgument.players()).executes {
-                setGameMode(it, EntityArgument.getPlayers(it, "target"), GameType.CREATIVE)
-            }
-        ).also { dispatcher.register(it) }
-        Commands.literal("gms").executes {
-            setGameMode(it, setOf(it.source.asPlayer()), GameType.SURVIVAL)
-        }.then(
-            Commands.argument("target", EntityArgument.players()).executes {
-                setGameMode(it, EntityArgument.getPlayers(it, "target"), GameType.SURVIVAL)
-            }
-        ).also { dispatcher.register(it) }
-        Commands.literal("gma").executes {
-            setGameMode(it, setOf(it.source.asPlayer()), GameType.ADVENTURE)
-        }.then(
-            Commands.argument("target", EntityArgument.players()).executes {
-                setGameMode(it, EntityArgument.getPlayers(it, "target"), GameType.ADVENTURE)
-            }
-        ).also { dispatcher.register(it) }
-        Commands.literal("gmsp").executes {
-            setGameMode(it, setOf(it.source.asPlayer()), GameType.SPECTATOR)
-        }.then(
-            Commands.argument("target", EntityArgument.players()).executes {
-                setGameMode(it, EntityArgument.getPlayers(it, "target"), GameType.SPECTATOR)
-            }
-        ).also { dispatcher.register(it) }
-    }
-
-    fun register(dispatcher: CommandDispatcher<CommandSource>) {
-        CommandAPI.removeCommand("gamemode")
-        tryAssignAliases()
-        registerShortAliases(dispatcher)
-
-        aliases.forEach { command ->
-            val literal = Commands.literal(command)
-            GameType.values().forEach { type ->
-                if (type != GameType.NOT_SET) {
-                    literal.then(
-                        Commands.literal(type.getName()).executes {
-                            setGameMode(it, setOf(it.source.asPlayer()), type)
-                        }.then(
-                            Commands.argument(
-                                "target", EntityArgument.players()
-                            ).executes {
-                                setGameMode(it, EntityArgument.getPlayers(it, "target"), type)
-                            }
-                        )
-                    )
-                }
-            }
-            dispatcher.register(literal)
+internal object GameModeCommand : VanillaCommandBase("gamemode") {
+    override fun register(dispatcher: CommandDispatcher<CommandSource>) {
+        super.register(dispatcher).also { aliases() }
+        GameType.values().forEach {
+            if (!it.name.startsWith("sp")) short("gm${it.getName()[0]}", it) else short("gmsp", it)
         }
-    }
-
-    private fun checkPermissions(source: CommandSource, gameTypeIn: GameType) {
-        try {
-            if (!hasPermission(source.asPlayer(), "native.gamemode.${gameTypeIn.getName()}", 2)) {
-                throw CommandException(
-                    textComponentFrom(
-                        source.asPlayer(),
-                        generalConfiguration.getBool(SETTING_LOC_ENABLED),
-                        "native.command.restricted"
-                    ).setStyle(
-                        Style().setHoverEvent(
-                            hoverEventFrom(
-                                source.asPlayer(),
-                                generalConfiguration.getBool(SETTING_LOC_ENABLED),
-                                "native.command.restricted_hover",
-                                "native.gamemode.${gameTypeIn.getName()}", "2"
+        val literal = Commands.literal(name)
+        GameType.values().forEach { type ->
+            if (type != GameType.NOT_SET) {
+                literal.then(
+                    Commands.literal(type.getName()).executes {
+                        checkPermissions(it.source, type, false)
+                        GameModeCommand.setGameMode(it, setOf(it.source.asPlayer()), type)
+                    }.then(
+                        Commands.argument("target", EntityArgument.players()).executes {
+                            checkPermissions(it.source, type, true)
+                            GameModeCommand.setGameMode(
+                                it, EntityArgument.getPlayers(it, "target"), type
                             )
-                        )
+                        }
                     )
                 )
             }
-        } catch (_: CommandSyntaxException) {
-            // Suppressed, because command executed by server.
+        }.also { dispatcher.register(literal) }
+    }
+
+    private fun aliases() {
+        CommandAliases.aliases["gamemode"]?.addAll(listOf("gmc", "gms", "gma", "gmsp")) ?: run {
+            CommandAliases.aliases.put("gamemode", mutableListOf("gmc", "gms", "gma", "gmsp"))
         }
     }
 
-    private fun sendGameModeFeedback(
-        source: CommandSource,
-        player: ServerPlayerEntity,
-        gameTypeIn: GameType
-    ) {
-        val component = TranslationTextComponent("gameMode." + gameTypeIn.getName())
-        if (source.entity === player) {
-            source.sendFeedback(
-                TranslationTextComponent("commands.gamemode.success.self", component), false
-            )
-        } else {
-            if (source.world.gameRules.getBoolean(GameRules.SEND_COMMAND_FEEDBACK)) {
-                player.sendMessage(TranslationTextComponent("gameMode.changed", component))
+    private fun short(short: String, mode: GameType) {
+        Commands.literal(short).executes {
+            checkPermissions(it.source, mode, false)
+            GameModeCommand.setGameMode(it, setOf(it.source.asPlayer()), mode)
+        }.then(
+            Commands.argument("target", EntityArgument.players()).executes {
+                checkPermissions(it.source, mode, true)
+                GameModeCommand.setGameMode(it, EntityArgument.getPlayers(it, "target"), mode)
             }
-            source.sendFeedback(
-                TranslationTextComponent(
-                    "commands.gamemode.success.other", player.displayName, component
-                ), false
-            )
-        }
+        ).also { CommandAPI.getDispatcher().register(it) }
     }
 
-    private fun setGameMode(
-        source: CommandContext<CommandSource>,
-        players: Collection<ServerPlayerEntity>,
-        gameTypeIn: GameType
-    ): Int {
-        checkPermissions(source.source, gameTypeIn)
-        var i = 0
-        players.forEach {
-            if (it.interactionManager.gameType != gameTypeIn) {
-                it.setGameType(gameTypeIn)
-                sendGameModeFeedback(source.source, it, gameTypeIn)
-                ++i
-            }
+    private fun checkPermissions(source: CommandSource, gameTypeIn: GameType, other: Boolean) {
+        if (source !is ServerPlayerEntity) return
+        if (!hasPermission(
+                source.asPlayer(),
+                "native.gamemode.${gameTypeIn.getName()}${if (other) ".other" else ""}",
+                if (other) 3 else 2
+            )
+        ) {
+            throw CommandException(
+                textComponentFrom(
+                    source.asPlayer(),
+                    generalConfiguration.getBool(SETTING_LOC_ENABLED),
+                    "native.command.restricted"
+                ).setStyle(
+                    Style().setHoverEvent(
+                        hoverEventFrom(
+                            source.asPlayer(),
+                            generalConfiguration.getBool(SETTING_LOC_ENABLED),
+                            "native.command.restricted_hover",
+                            "native.gamemode.${gameTypeIn.getName()}${if (other) ".other" else ""}",
+                            if (other) "3" else "2"
+                        )
+                    )
+                )
+            )
         }
-        return i
     }
 }
 
