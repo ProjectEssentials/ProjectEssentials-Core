e29e1d6c78d42d2e48d915acc11c23cfca7b25ff
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-10 19:33:54 +0300

    SaveAllCommand.kt re-written.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/impl/vanilla/commands/SaveAllCommand.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/impl/vanilla/commands/SaveAllCommand.kt
@@ -1,104 +1,32 @@
 /**
- * This command implementation by Mojang.
- * And decompiled with idea source code was converted
- * to kotlin code.
- * Also added some logic, for example checking on
- * permissions, and for some commands shorten aliases.
+ * ! This command implementation by Mojang Game Studios!
+ *
+ * Decompiled with idea source code was converted to kotlin code.
+ * But with additions such as permissions checking and etc.
  *
  * 1. This can be bad code.
  * 2. This file can be not formatter pretty.
  */
-
 package com.mairwunnx.projectessentials.core.impl.vanilla.commands
 
-
-import com.mairwunnx.projectessentials.core.api.v1.SETTING_LOC_ENABLED
-import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAPI
-import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAliases
 import com.mairwunnx.projectessentials.core.api.v1.configuration.ConfigurationAPI
-import com.mairwunnx.projectessentials.core.api.v1.extensions.hoverEventFrom
-import com.mairwunnx.projectessentials.core.api.v1.extensions.textComponentFrom
-import com.mairwunnx.projectessentials.core.api.v1.permissions.hasPermission
 import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.exceptions.CommandSyntaxException
-import com.mojang.brigadier.exceptions.SimpleCommandExceptionType
-import net.minecraft.command.CommandException
 import net.minecraft.command.CommandSource
 import net.minecraft.command.Commands
-import net.minecraft.util.text.Style
-import net.minecraft.util.text.TranslationTextComponent
+import net.minecraft.command.impl.SaveAllCommand
 
-internal object SaveAllCommand : VanillaCommandBase() {
-    private val FAILED_EXCEPTION = SimpleCommandExceptionType(
-        TranslationTextComponent("commands.save.failed")
-    )
-
-    private var aliases =
-        configuration.take().aliases.saveall + "save-all"
-
-    private fun tryAssignAliases() {
-        CommandAliases.aliases["save-all"] = aliases.toMutableList()
-    }
-
-    fun register(dispatcher: CommandDispatcher<CommandSource>) {
-        CommandAPI.removeCommand("save-all")
-        tryAssignAliases()
-
-        aliases.forEach { command ->
-            dispatcher.register(
-                Commands.literal(command).executes { p_198610_0_ ->
-                    saveAll(p_198610_0_.source, false)
-                }.then(
-                    Commands.literal("flush").executes { p_198613_0_ ->
-                        saveAll(p_198613_0_.source, true)
-                    }
-                )
+internal object SaveAllCommand : VanillaCommandBase("save-all") {
+    override fun register(dispatcher: CommandDispatcher<CommandSource>) {
+        dispatcher.register(
+            Commands.literal(name).executes {
+                ConfigurationAPI.saveAll()
+                SaveAllCommand.saveAll(it.source, false)
+            }.then(
+                Commands.literal("flush").executes {
+                    ConfigurationAPI.saveAll()
+                    SaveAllCommand.saveAll(it.source, true)
+                }
             )
-        }
-    }
-
-    private fun checkPermissions(source: CommandSource) {
-        try {
-            if (!hasPermission(source.asPlayer(), "native.save.all", 4)) {
-                throw CommandException(
-                    textComponentFrom(
-                        source.asPlayer(),
-                        generalConfiguration.getBool(SETTING_LOC_ENABLED),
-                        "native.command.restricted"
-                    ).setStyle(
-                        Style().setHoverEvent(
-                            hoverEventFrom(
-                                source.asPlayer(),
-                                generalConfiguration.getBool(SETTING_LOC_ENABLED),
-                                "native.command.restricted_hover",
-                                "native.save.all", "4"
-                            )
-                        )
-                    )
-                )
-            }
-        } catch (e: CommandSyntaxException) {
-            // ignored, because command executed by server.
-        }
-    }
-
-    @Throws(CommandSyntaxException::class)
-    private fun saveAll(source: CommandSource, flush: Boolean): Int {
-        checkPermissions(source)
-        source.sendFeedback(TranslationTextComponent("commands.save.saving"), false)
-        val minecraftserver = source.server
-        ConfigurationAPI.saveAll()
-        minecraftserver.playerList.saveAllPlayerData()
-        val flag = minecraftserver.save(true, flush, true)
-        return if (!flag) {
-            throw FAILED_EXCEPTION.create()
-        } else {
-            source.sendFeedback(
-                TranslationTextComponent(
-                    "commands.save.success"
-                ), true
-            )
-            1
-        }
+        )
     }
 }
