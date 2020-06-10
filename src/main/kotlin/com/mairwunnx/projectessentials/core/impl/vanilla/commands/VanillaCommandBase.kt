8def8014df77958d04b5be2ee98dddf933fba574
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-06-10 22:21:43 +0300

    `isAllowed` added to VanillaCommandBase.kt.
    Signed-off-by: Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>

--- src/main/kotlin/com/mairwunnx/projectessentials/core/impl/vanilla/commands/VanillaCommandBase.kt
+++ src/main/kotlin/com/mairwunnx/projectessentials/core/impl/vanilla/commands/VanillaCommandBase.kt
@@ -1,11 +1,23 @@
 package com.mairwunnx.projectessentials.core.impl.vanilla.commands
 
 import com.mairwunnx.projectessentials.core.api.v1.commands.CommandAPI
+import com.mairwunnx.projectessentials.core.api.v1.permissions.hasPermission
 import com.mojang.brigadier.CommandDispatcher
 import net.minecraft.command.CommandSource
+import net.minecraft.entity.player.ServerPlayerEntity
+import org.apache.logging.log4j.LogManager
 
 abstract class VanillaCommandBase(val name: String) {
     open fun register(dispatcher: CommandDispatcher<CommandSource>) {
         CommandAPI.removeCommand(name)
+        LogManager.getLogger().info("Native command replacing implementation for $name")
+    }
+
+    companion object {
+        @JvmStatic
+        fun isAllowed(source: CommandSource, node: String, opLevel: Int) =
+            if (source.entity is ServerPlayerEntity) {
+                hasPermission(source.asPlayer(), "native.$node", opLevel)
+            } else true
     }
 }
