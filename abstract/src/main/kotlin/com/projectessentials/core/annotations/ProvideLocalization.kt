b434df4f2a0661ba303c686bd66b50b91290c812
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-08-25 21:51:20 +0300

    `ProvideLocalization.kt` annotation created.

--- abstract/src/main/kotlin/com/projectessentials/core/annotations/ProvideLocalization.kt
+++ abstract/src/main/kotlin/com/projectessentials/core/annotations/ProvideLocalization.kt
@@ -1,4 +1,14 @@
 package com.projectessentials.core.annotations
 
-class ProvideLocalization {
-}
+/**
+ * Annotate with this annotation project essentials
+ * module, that annotation will alive only at compile
+ * time.
+ *
+ * Need for collect localizations from module.
+ *
+ * @since 3.0.0.
+ * @property id name of module wanted to process localization.
+ * @property langs languages what can used in module.
+ */
+annotation class ProvideLocalization(val id: String, val langs: Array<String>)
