5135c36567edccca0237cfdccd08f617032e1c3c
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-08-28 23:22:51 +0300

    Asynchronous contract functions created at `IPermissionsService.kt`. `restricted` functional argument added to `performIfCan`.

--- abstract/src/main/kotlin/com/projectessentials/core/services/IPermissionsService.kt
+++ abstract/src/main/kotlin/com/projectessentials/core/services/IPermissionsService.kt
@@ -1,5 +1,7 @@
 package com.projectessentials.core.services
 
+import kotlinx.coroutines.Deferred
+
 /**
  * Permissions service contract, for interacting
  * with player permissions.
@@ -9,48 +11,75 @@ package com.projectessentials.core.services
  */
 interface IPermissionsService<TVerifiable> {
     /**
+     * Synchronously version of checking permissions, check also
+     * [hasPermissionAsync] for calling this asynchronously.
+     *
      * @param player verifiable generic type to check his permission.
      * @param node permission node to check permissions.
      * @param opLevel fallback operator level, used if possessor didn't
      * have permission node.
      * @return true if verifiable have requested permission node.
      * @since 3.0.0.
+     * @see hasPermissionAsync
      */
     fun hasPermission(player: TVerifiable, node: String, opLevel: Int = 0): Boolean
 
     /**
-     * Performs action if [TVerifiable] has requested permission node.
+     * Asynchronously version of checking permissions, check also
+     * [hasPermission] for calling this synchronously.
      *
      * @param player verifiable generic type to check his permission.
      * @param node permission node to check permissions.
      * @param opLevel fallback operator level, used if possessor didn't
      * have permission node.
-     * @param action action to perform if verifiable has permission.
-     * @return [TResult] result if it need.
+     * @return deferred true if verifiable have requested permission node.
      * @since 3.0.0.
+     * @see hasPermission
      */
-    fun <TResult> performIfCan(
-        player: TVerifiable, node: String, opLevel: Int = 0, action: (TVerifiable) -> TResult
-    ): TResult
+    suspend fun hasPermissionAsync(
+        player: TVerifiable, node: String, opLevel: Int = 0
+    ): Deferred<Boolean>
 
     /**
-     * Performs action if [TVerifiable] has requested permission node otherwise
-     * performs [restricted] action if verifiable didn't have permission.
+     * Performs action if [TVerifiable] has requested permission node.
+     * Asynchronous version of this method [performIfCanAsync].
      *
      * @param player verifiable generic type to check his permission.
      * @param node permission node to check permissions.
      * @param opLevel fallback operator level, used if possessor didn't
      * have permission node.
-     * @param restricted action to perform if verifiable has no permission.
      * @param action action to perform if verifiable has permission.
      * @return [TResult] result if it need.
      * @since 3.0.0.
+     * @see performIfCanAsync
      */
     fun <TResult> performIfCan(
         player: TVerifiable,
         node: String,
         opLevel: Int = 0,
-        restricted: (TVerifiable) -> TResult,
         action: (TVerifiable) -> TResult,
+        restricted: ((TVerifiable) -> TResult)? = null
     ): TResult
+
+    /**
+     * Performs suspended action if [TVerifiable] has requested permission node.
+     * Synchronous version of this method [performIfCan].
+     *
+     * @param player verifiable generic type to check his permission.
+     * @param node permission node to check permissions.
+     * @param opLevel fallback operator level, used if possessor didn't
+     * have permission node.
+     * @param action action to perform if verifiable has permission.
+     * @param restricted action to perform if verifiable has no permission.
+     * @return [TResult] result if it need.
+     * @since 3.0.0.
+     * @see performIfCan
+     */
+    suspend fun <TResult> performIfCanAsync(
+        player: TVerifiable,
+        node: String,
+        opLevel: Int = 0,
+        action: (TVerifiable) -> TResult,
+        restricted: ((TVerifiable) -> TResult)? = null
+    ): Deferred<TResult>
 }
