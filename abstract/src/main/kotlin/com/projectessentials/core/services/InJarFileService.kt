47ca9713b8527004f1ca7ef2d04b9455308eaf17
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-09-02 21:04:30 +0300

    `InJarFileService.kt` service contract created.

---
+++ abstract/src/main/kotlin/com/projectessentials/core/services/InJarFileService.kt
@@ -0,0 +1,24 @@
+package com.projectessentials.core.services
+
+import java.io.File
+
+/**
+ * In jar file service contract, for interacting with
+ * files in jar contained.
+ *
+ * @since 3.0.0.
+ */
+interface InJarFileService {
+    /**
+     * Returns resource as file by specified class loader
+     * and file path.
+     *
+     * @param loader class loader of class jar owner which
+     * contains [path] resource.
+     * @param path path to the file in jar.
+     * @return [File] class instance if class exist, otherwise
+     * null.
+     * @since 3.0.0.
+     */
+    fun getResourceAsFile(loader: ClassLoader, path: String): File?
+}
