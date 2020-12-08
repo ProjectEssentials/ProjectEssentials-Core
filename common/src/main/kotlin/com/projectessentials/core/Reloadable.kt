d801fe7494133f23e8fab9d95fadc09640536a23
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-12-09 00:01:02 +0300

    Reloadable.kt documentation written.

--- common/src/main/kotlin/com/projectessentials/core/Reloadable.kt
+++ common/src/main/kotlin/com/projectessentials/core/Reloadable.kt
@@ -1,5 +1,17 @@
 package com.projectessentials.core
 
+/**
+ * Implementation contract marks the object
+ * is reloadable, and contains one [reload]
+ * method.
+ *
+ * @since 3.0.0
+ */
 interface Reloadable {
+    /**
+     * Performs reload action for this object.
+     *
+     * @since 3.0.0
+     */
     fun reload()
 }
