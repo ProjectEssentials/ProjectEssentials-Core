2de35704fc012a3acc9967a11c6e0b86cc6c8f0c
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-12-20 14:08:19 +0300

    Meta.kt `fields` method implemented.

--- common/src/main/kotlin/com/projectessentials/core/Meta.kt
+++ common/src/main/kotlin/com/projectessentials/core/Meta.kt
@@ -2,6 +2,8 @@
 
 package com.projectessentials.core
 
+import java.lang.reflect.Field
+
 /**
  * Return the version of this implementation. It consists of
  * any string assigned by the vendor of this implementation
@@ -27,3 +29,23 @@ inline fun <reified T> T.implVersion() =
  * null returns `unknown`.
  */
 inline fun <reified T> T.implName() = this!!::class.java.`package`.implementationTitle ?: "unknown"
+
+/**
+ * Return the fields of class passed in generic to this
+ * method.
+ *
+ * @since 3.0.0
+ * @receiver `Class<*>` any java class as receiver.
+ * @return immutable list of [Field] object.
+ */
+@OptIn(ExperimentalStdlibApi::class)
+inline fun <reified T> T.fields(): List<Field> {
+    buildList<Field> {
+        var c: Class<*>? = this::class.java
+        while (c != null) {
+            addAll(c.declaredFields)
+            c = c.superclass
+        }
+        return this
+    }
+}
